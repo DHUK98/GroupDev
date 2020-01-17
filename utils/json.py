da5b89ffd8ae3c16cf611f9c150f36b1ed62e3af
Max W <mejw201@exeter.ac.uk>
2020-01-17 00:11:50 +0000

    Wrote netCDF to json utils converter

--- utils/json.py
+++ utils/json.py
@@ -1,3 +1,44 @@
 '''
 Module for converting data to/from json
 '''
+
+from netCDF4 import Dataset
+import os.path
+import json
+
+
+def json_from_netcdf_file(filepath):
+    """
+    First part is from old reader.py file.
+    We can either replace that with a call to the initialise function in reader
+    or just use these util files and render reader obsolete
+    """
+
+    f = Dataset(filepath, "r", format="NETCDF4")
+
+    # # read in variables
+    mt = f.variables['time']
+    lat = f.variables['latitude']
+    lon = f.variables['longitude']
+    height = f.variables['height']
+    pressure = f.variables['pressure']
+
+    time = mt[:]
+    lat = lat[:]
+    lon = lon[:]
+    height = height[:]
+    pressure = pressure[:]
+
+    # .tolist() function takes a few sections but is needed to make the np.MaskedArray type JSON serialisable
+    data_dict = {"time" : time.tolist(), "lat" : lat.tolist(), "lon" : lon.tolist(), "height" : height.tolist(), "pressure" : pressure.tolist()}
+    data_json = json.dumps(data_dict, sort_keys=True)
+
+    f.close()
+
+    return data_json
+
+
+if __name__ == "__main__":
+    my_path = os.path.abspath(os.path.dirname(__file__))
+    path = os.path.join(my_path, "../data/ERA-Interim_1degree_CapeGrim_100m_2016_hourly.nc")
+    json_output = json_from_netcdf_file(path)
