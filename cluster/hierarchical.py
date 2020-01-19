1d6bc1ce255bd12683ee7ad625d5176d84e06ea1
hugowickham <hdew201@exeter.ac.uk>
2020-01-19 18:01:25 +0000

    Added ability to store linkage matrix as binary data to text file (linkage.txt, it's in gitignore) and then cluster after loading it again

--- cluster/hierarchical.py
+++ cluster/hierarchical.py
@@ -1,57 +1,45 @@
 from matplotlib import pyplot as plt
-from scipy.cluster.hierarchy import dendrogram, linkage
+from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
 import numpy as np
 from cluster import cluster_utils
 import reader.NovClim.reader as rd
+import pickle
 
-def fancy_dendrogram(*args, **kwargs):
-    max_d = kwargs.pop('max_d', None)
-    if max_d and 'color_threshold' not in kwargs:
-        kwargs['color_threshold'] = max_d
-    annotate_above = kwargs.pop('annotate_above', 0)
 
-    ddata = dendrogram(*args, **kwargs)
+def store_linkage(X):
+    #Store linkage matrix of passed data to be later used in clustering
 
-    if not kwargs.get('no_plot', False):
-        plt.title('Hierarchical Clustering Dendrogram (truncated)')
-        plt.xlabel('sample index or (cluster size)')
-        plt.ylabel('distance')
-        for i, d, c in zip(ddata['icoord'], ddata['dcoord'], ddata['color_list']):
-            x = 0.5 * sum(i[1:3])
-            y = d[1]
-            if y > annotate_above:
-                plt.plot(x, y, 'o', c=c)
-                plt.annotate("%.3g" % y, (x, y), xytext=(0, -5),
-                             textcoords='offset points',
-                             va='top', ha='center')
-        if max_d:
-            plt.axhline(y=max_d, c='k')
-    return ddata
+    #Get linkage matrix
+    Z = linkage(X, 'ward')
+
+    file = open('linkage.txt', 'wb')
+    pickle.dump(Z, file)
+    file.close()
+
+def open_linkage():
+    #Open file containing most recently stored linkage matrix
+
+    file = open('linkage.txt', 'rb')
+    Z = pickle.load(file)
+
+    return Z
+
 
 if __name__ == "__main__":
 
     time, lat, lon, height, pressure = rd.initialise(
             "../data/Trajectories/ERA-Interim_1degree_CapeGrim_100m_2016_hourly.nc")
 
-    x = cluster_utils.toVector(lat, lon)
+    X = cluster_utils.toVector(lat, lon)
 
-    X = x[:100]
+    store_linkage(X)
 
-    Z = linkage(X, 'ward')
+    Z = open_linkage()
 
-    # calculate full dendrogram
-    plt.figure(figsize=(25, 10))
-    plt.title('Hierarchical Clustering Dendrogram')
-    plt.xlabel('sample index')
-    plt.ylabel('distance')
-    fancy_dendrogram(
-        Z,
-        truncate_mode='lastp',  # show only the last p merged clusters
-        p=12,  # show only the last p merged clusters
-        show_leaf_counts=False,  # otherwise numbers in brackets are counts
-        leaf_rotation=90.,
-        leaf_font_size=12.,
-        show_contracted=True,  # to get a distribution impression in truncated branches
-        max_d=1500
-    )
-    plt.show()
+    k = 4
+    clusters = fcluster(Z, k, criterion='maxclust')
+
+    for i in clusters:
+        print(i)
+
+
