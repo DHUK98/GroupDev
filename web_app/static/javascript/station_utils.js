dd4f99e3ee58c030acb24f6eff87051b7a3ac426
DHUK98 <dh450@exeter.ac.uk>
2020-02-14 15:02:26 +0000

    log to div

--- web_app/static/javascript/station_utils.js
+++ web_app/static/javascript/station_utils.js
@@ -47,7 +47,17 @@ function u_cluster(data_l, num_clust) {
     });
 
 }
+if (typeof console  != "undefined")
+    if (typeof console.log != 'undefined')
+        console.olog = console.log;
+    else
+        console.olog = function() {};
 
+console.log = function(message) {
+    console.olog(message);
+    $('#debugDiv').append('<p>' + message + '</p>');
+};
+console.error = console.debug = console.info =  console.log
 function calculate() {
     document.getElementById("calculate").value= "Loading";
 
