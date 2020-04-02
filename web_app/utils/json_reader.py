161cae7c84a2965b60a3498a83f8f761611603ce
Max W <mejw201@exeter.ac.uk>
2020-04-02 01:54:22 +0100

    I'm back baby

--- web_app/utils/json_reader.py
+++ web_app/utils/json_reader.py
@@ -29,16 +29,18 @@ def json_from_netcdf_file(filepath, with_keys=False):
     return data_json
 
 
-# path = '../static/stations/CGR/'
-# files = []
-# for file in os.listdir(path):
-#     if file.endswith(".nc"):
-#         files.append(file)
-#
-# for f in files:
-#     test, keys = json_from_netcdf_file("../static/stations/CGR/" + f, with_keys=True)
-#     json_f = f.replace(".nc", ".json")
-#     with open("../static/stations/CGR/" + json_f, 'w') as outfile:
-#         outfile.write(test)
-#     with open("../static/stations/CGR/keys_" + json_f, 'w') as outfile:
-#         outfile.write(keys)
+
+if __name__ == "__main__":
+    path = '../static/stations/CGR/'
+    files = []
+    for file in os.listdir(path):
+        if file.endswith(".nc"):
+            files.append(file)
+
+    for f in files:
+        test, keys = json_from_netcdf_file("../static/stations/CGR/" + f, with_keys=True)
+        json_f = f.replace(".nc", ".json")
+        with open("../static/stations/CGR/" + json_f, 'w') as outfile:
+            outfile.write(test)
+        with open("../static/stations/CGR/keys_" + json_f, 'w') as outfile:
+            outfile.write(keys)
