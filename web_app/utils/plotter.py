55fd5f26580be8ad229ab3429335e419e9da6a36
DHUK98 <dh450@exeter.ac.uk>
2020-04-06 17:17:33 +0100

    added test cluster plot

--- web_app/utils/plotter.py
+++ web_app/utils/plotter.py
@@ -1,6 +1,3 @@
-import io
-
-from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 from flask import session
 import ujson as json
@@ -9,21 +6,18 @@ import ujson as json
 def plot_svg():
     """ renders the plot on the fly.
     """
-    fig, axs = plt.subplots(2, 3,figsize=(15,10))
+    fig = plt.figure(figsize=(4, 4))
     data = json.loads(session.get("data"))
     xs = []
     ys = []
-    for i in range(0,600,100):
+    for i in range(0, 600, 100):
         ys.append(data["height"][i])
-        xs.append(data["time"][i])
+        xs.append(range(240, -1, -1))
+    plt.xlabel("Time (Hours)")
+    plt.ylabel("Height")
 
-    for i in range(2):
-        for j in range(3):
-            axs[i, j].set_title('Cluster ' + str(i+j))
-            axs[i, j].plot(xs[i+j], ys[i+j])
-
-    fig.tight_layout(pad=2.5)
-
-    for ax in axs.flat:
-        ax.set(xlabel='time', ylabel='heigt')
+    for j in range(len(xs)):
+        plt.plot(xs[j], ys[j],label="cluster " + str(j+1))
+    plt.tight_layout()
+    plt.legend()
     return fig
